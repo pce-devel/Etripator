ca2c87b08996b15acc35b553424b0553378b1e7a
Vincent "MooZ" Cruz <cruz.vincent@gmail.com>
2011-05-02 21:30:11 +0200

    Forgot to commit the previously mentioned file.

---
+++ config.h
@@ -0,0 +1,32 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define _GNU_SOURCE
+
+#ifdef _MSC_VER
+#pragma warning(disable : 4996)
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#if defined(_MSC_VER)
+#include "platform/windows/getopt_win.h"
+#include "platform/windows/winmmap.h"
+#include "platform/windows/stdint.h"
+#else
+#include <unistd.h>
+#include <getopt.h>
+#include <stdint.h>
+#include <sys/mman.h>
+#endif
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <string.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <ctype.h>
+
+#endif // CONFIG_H
