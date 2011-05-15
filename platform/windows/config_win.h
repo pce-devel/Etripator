fb3c8528fc9368a8a8299e32d93755a2439145d2
Vincent "MooZ" Cruz <cruz.vincent@gmail.com>
2011-05-15 21:14:31 +0200

    Added a dedicated include file for windows.

--- platform/windows/config_win.h
+++ platform/windows/config_win.h
@@ -1,10 +1,9 @@
-#ifndef CONFIG_H
-#define CONFIG_H
+#ifndef CONFIG_WIN_H
+#define CONFIG_WIN_H
 
-#if defined(_MSC_VER)
-#include "platform/windows/config_win.h"
-#else
 #define _GNU_SOURCE
+
+#pragma warning(disable : 4996)
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -12,10 +11,8 @@
 #define strncasecmp _strnicmp
 #define strcasecmp _stricmp
 
-#include <unistd.h>
-#include <getopt.h>
-#include <stdint.h>
-#include <sys/mman.h>
+#include "getopt_win.h"
+#include "stdint.h"
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -24,6 +21,5 @@
 #include <fcntl.h>
 #include <limits.h>
 #include <ctype.h>
-#endif
 
-#endif // CONFIG_H
+#endif // CONFIG_WIN_H
