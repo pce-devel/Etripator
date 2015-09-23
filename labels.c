ecb93eee193f229f9cfb93865498863f670805fb
BlockoS <cruz.vincent@gmail.com>
2015-09-23 20:25:18 +0000

    Added new label sorting function... WIP/untested

--- labels.c
+++ labels.c
@@ -110,20 +110,15 @@ int pushLabel(LabelRepository* iRepository, uint16_t iOffset, const char* name)
             return 0;
         }	
         iRepository->labels = ptr;
-
-            return 0;
-        }
     }
     /* Push offset */
     iRepository->labels[iRepository->last].offset       = iOffset;
     iRepository->labels[iRepository->last].displacement = 0;
-
     if(0 == addLabelName(iRepository, &iRepository->labels[iRepository->last], name))
     {
         deleteLabelRepository(iRepository);
         return 0;
     }
-
     ++iRepository->last;
     return 1;
 }
@@ -137,6 +132,199 @@ static int compareLabels(const void* iL0, const void* iL1)
 	return (l0->offset - l1->offset);
 }
 
+#define CSWAP(i,j) \
+{ \
+    size_t x = repository->sorted[i]; \
+    size_t a = repository->labels[x].offset; \
+    size_t y = repository->sorted[j]; \
+    size_t b = repository->labels[y].offset; \
+    repository->sorted[i]  = (b < a) ? x : y; \
+    repository->sorted[j] ^= repository->sorted[i] ^ x; \
+}
+
+/* 3-way partition */
+static void partition(LabelRepository* repository, size_t lo, size_t hi, size_t *lt, size_t *gt)
+{
+    size_t i, tmp;
+    if(repository->labels[repository->sorted[hi]].offset < repository->labels[repository->sorted[lo]].offset)
+    {
+        tmp = repository->sorted[hi];
+        repository->sorted[hi] = repository->sorted[lo];
+        repository->sorted[lo] = i;
+    }
+    *lt = lo+1;
+    *gt = hi-1;
+    i = *lt;
+    
+    while(i <= *gt)
+    {
+        if(repository->labels[repository->sorted[i]].offset <  repository->labels[repository->sorted[lo]].offset)
+        {
+           tmp = repository->sorted[*lt];
+           repository->sorted[*lt] = repository->sorted[i]; 
+           repository->sorted[i]   = tmp;
+            ++*lt;
+        }
+        else if(repository->labels[repository->sorted[hi]].offset <  repository->labels[repository->sorted[i]].offset)
+        {
+            while((repository->labels[repository->sorted[*gt]].offset > repository->labels[repository->sorted[hi]].offset) && (i < *gt))
+            {
+                --*gt;
+            }
+           tmp = repository->sorted[*gt];
+           repository->sorted[*gt] = repository->sorted[i]; 
+           repository->sorted[i]   = tmp;
+
+            --*gt;
+            if(repository->labels[repository->sorted[i]].offset <  repository->labels[repository->sorted[lo]].offset)
+            {
+               tmp = repository->sorted[*lt]; 
+               repository->sorted[*lt] = repository->sorted[i]; 
+               repository->sorted[i]   = tmp;
+                ++*lt;
+            }
+        }
+        ++i;
+    }
+
+    --*lt;
+    ++*gt;
+
+    tmp = repository->sorted[*lt];
+    repository->sorted[*lt] = repository->sorted[lo]; 
+    repository->sorted[ lo]   = tmp;
+
+    tmp = repository->sorted[*gt]; 
+    repository->sorted[*gt] = repository->sorted[hi]; 
+    repository->sorted[ hi]   = tmp;
+}
+
+/* Sort label repository */
+static void sortLabelRepository(LabelRepository* repository, size_t lo, size_t hi)
+{
+    size_t len = hi - lo + 1;
+    switch(len)
+    {
+        case 0:
+        case 1:
+            break;
+        case 2:
+            CSWAP(0,1);
+            break;
+        case 3:
+            CSWAP(0,2);
+            CSWAP(0,1);
+            CSWAP(1,2);
+            break;
+        case 4:
+            CSWAP(0, 2)
+            CSWAP(1, 3)
+            CSWAP(0, 1)
+            CSWAP(2, 3)
+            CSWAP(1, 2)
+            break;
+        case 5:
+            CSWAP(0, 1)
+            CSWAP(3, 4)
+            CSWAP(2, 4)
+            CSWAP(2, 3)
+            CSWAP(1, 4)
+            CSWAP(1, 2)
+            CSWAP(0, 3)
+            CSWAP(0, 1)
+            CSWAP(2, 3)
+            break;
+        case 6:
+            CSWAP(0, 4)
+            CSWAP(1, 5)
+            CSWAP(0, 2)
+            CSWAP(1, 3)
+            CSWAP(2, 4)
+            CSWAP(3, 5)
+            CSWAP(0, 1)
+            CSWAP(2, 3)
+            CSWAP(4, 5)
+            CSWAP(1, 4)
+            CSWAP(1, 2)
+            CSWAP(3, 4)
+            break;
+        case 7:
+            CSWAP(0, 4)
+            CSWAP(1, 5)
+            CSWAP(2, 6)
+            CSWAP(0, 2)
+            CSWAP(1, 3)
+            CSWAP(4, 6)
+            CSWAP(2, 4)
+            CSWAP(3, 5)
+            CSWAP(0, 1)
+            CSWAP(2, 3)
+            CSWAP(4, 5)
+            CSWAP(1, 4)
+            CSWAP(3, 6)
+            CSWAP(1, 2)
+            CSWAP(3, 4)
+            CSWAP(5, 6)
+            break;
+        case 8:
+            CSWAP(0, 4)
+            CSWAP(1, 5)
+            CSWAP(2, 6)
+            CSWAP(3, 7)
+            CSWAP(0, 2)
+            CSWAP(1, 3)
+            CSWAP(4, 6)
+            CSWAP(5, 7)
+            CSWAP(2, 4)
+            CSWAP(3, 5)
+            CSWAP(0, 1)
+            CSWAP(2, 3)
+            CSWAP(4, 5)
+            CSWAP(6, 7)
+            CSWAP(1, 4)
+            CSWAP(3, 6)
+            CSWAP(1, 2)
+            CSWAP(3, 4)
+            CSWAP(5, 6)
+            break;    
+        default:
+            if(repository->last < 17)
+            {
+                size_t i;
+                /* Insertion sort */
+                for(i=lo+1; i<=hi; i++)
+                {
+                    size_t j;
+                    size_t k = repository->sorted[i];
+                    size_t o = repository->labels[k].offset;
+                    for(j=i; (j>lo) && (repository->labels[repository->sorted[j-1]].offset > o); j--)
+                    {
+                        repository->sorted[j] = repository->sorted[j-1];
+                    }
+                    repository->sorted[j] = k;
+                }
+            }
+            else
+            {
+                size_t lt, gt;
+                /* Quicksort */
+                partition(repository, lo, hi, &lt, &gt);
+                if(repository->labels[repository->sorted[lt]].offset < repository->labels[repository->sorted[gt]].offset)
+                {
+                    sortLabelRepository(repository, lt+1, gt-1);
+                    sortLabelRepository(repository, gt+1, hi  );
+                    sortLabelRepository(repository,   lo, lt-1);
+                }
+                else
+                {
+                    sortLabelRepository(repository, gt+1, hi  );
+                    sortLabelRepository(repository, lo  , lt-1);
+                }
+            }
+            break;
+    }
+}
+
 /* Finalize label repository */
 void finalizeLabelRepositoty(LabelRepository* iRepository)
 {
@@ -146,7 +334,7 @@ void finalizeLabelRepositoty(LabelRepository* iRepository)
     if((iRepository->labels == NULL) || (iRepository->last < 2))
         return;
         
-    tmp = (size_t*)realloc(iRepository->sorted, iRepository->size * size(size_t));
+    tmp = (size_t*)realloc(iRepository->sorted, iRepository->size * sizeof(size_t));
     if(NULL == tmp)
     {
         deleteLabelRepository(iRepository);
@@ -162,7 +350,6 @@ void finalizeLabelRepositoty(LabelRepository* iRepository)
     // [todo]
     qsort(iRepository->sorted, iRepository->last, sizeof(size_t), compareLabels);
 }
-
 
 /* Label CFG section start */
 static int labelBeginSectionCallback(void *data, const char *sectionName)
